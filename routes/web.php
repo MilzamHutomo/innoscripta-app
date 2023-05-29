7bd40f7b581e382001b9b97af643ff0391b09930
Milzam Hutomo <milzamhutomo.social@gmail.com>
2023-05-29 23:53:19 +0700

    WRM: Routes for Admin Dashboard and Workflow Template CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WorkflowTemplateController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -28,9 +30,11 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     
-    Route::get('dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+    Route::resource('workflow-template', WorkflowTemplateController::class)
+        ->only('store');
 
     // Route for Profile Page
     Route::get('profile', [ProfileController::class, 'edit'])
